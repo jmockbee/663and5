5ef30616517650d677304574488c30f43d03d107
Jon Mockbee <mockbee1000@gmail.com>
2019-07-24 11:14:42 -0400

    finished

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -3,17 +3,24 @@ package com.JonMockbee;
 public class Main {
 
     public static void main(String[] args) {
-    int count = 0;
-    
-        for (int i = 0;i >= 1; <=1000; i++ ){
-            if (i / 3 =)
 
+        int count = 0;
+        int sum = 0;
+
+        for(int i=1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                count++;
+                sum += i;
+                System.out.println("Found number = " + i);
+            }
+
+            if(count == 5) {
+                break;
+            }
         }
 
-        for (int i = 3; i <= n / 3; i++) {
-            System.out.println("looping");
-            if (n % i == 0) {
-                return false;
-            }
+        System.out.println("Sum = " + sum);
+
     }
 }
+
