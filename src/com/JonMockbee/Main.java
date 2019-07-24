be6c707388384123182072e5b054fc5d22a7e11d
Jon Mockbee <mockbee1000@gmail.com>
2019-07-24 10:54:05 -0400

    initial commit

---
+++ src/com/JonMockbee/Main.java
@@ -0,0 +1,19 @@
+package com.JonMockbee;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int count = 0;
+    
+        for (int i = 0;i >= 1; <=1000; i++ ){
+            if (i / 3 =)
+
+        }
+
+        for (int i = 3; i <= n / 3; i++) {
+            System.out.println("looping");
+            if (n % i == 0) {
+                return false;
+            }
+    }
+}
